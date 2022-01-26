a32b116590da6e1f45edef35185524baaf6688bc
Denis <82286878+DenisOrlando@users.noreply.github.com>
2022-01-26 00:41:32 -0300

    Added navbar position fixed

--- src/Components/Main/Main.tsx
+++ src/Components/Main/Main.tsx
@@ -5,9 +5,11 @@ import styles from "./Main.module.css";
 function Main() {
   return (
     <main className={styles.main}>
-      <h3 className={styles.h3}>Hi there! I'm</h3>
-      <h1 className={styles.h1}>DENIS FERREYRA</h1>
-      <h2 className={styles.h2}>Full Stack Developer</h2>
+      <div style={{marginTop:"80px"}}>
+        <h3 className={styles.h3}>Hi there! I'm</h3>
+        <h1 className={styles.h1}>DENIS FERREYRA</h1>
+        <h2 className={styles.h2}>Full Stack Developer</h2>
+      </div>
       <Cv />
       <Projects />
     </main>
